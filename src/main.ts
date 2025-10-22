eeb47a7bd489cbc67bee9b631543af248995a677
DaisyFragoso <87869666+DaisyFragoso@users.noreply.github.com>
2025-10-22 10:00:35 +0000

    (step 2) - added mouse listeners and button to clear canvas without removing the background color (aka original fillstyle & fillRect)

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,44 @@ import "./style.css";
 
 document.body.innerHTML = `
   <canvas id="canvas"></canvas>
+  <button id="clearButton">Clear</button>
 `;
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+canvas.width = 256;
+canvas.height = 256;
 
-if (ctx) {
+const ctx = canvas.getContext("2d")!;
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 256, 256);
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
+
+const clearButton = document.getElementById("clearButton")!;
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "green";
   ctx.fillRect(10, 10, 256, 256);
-}
+});
